c6d19274b6b55e7c70a65a978c484d70d87c933f
Erin Hancock <erinhancock794@Erins-MacBook-Pro.local>
2019-10-08 12:17:00 -0600

    added basic content

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 console.log('I am Javascript running in your page!'
 )
+
+let mainHeader = document.querySelector ('h1')
+let allHeaders = document.querySelectorAll ('h1')
+console.log(allHeaders)
+mainHeader.textContent = 'Hello!' 
+mainHeader.setAttribute("style", "color:red; border: 1px solid blue;");
+
+let newDiv = document.createElement("div"); 
+newDiv.textContent = "Here I am, a new Div."
+
+document.querySelector('main').setAttribute("style", "background-color: #dfdfdf;")
+
+
+document.querySelector('main').appendChild(newDiv);
